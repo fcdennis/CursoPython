68c296363d3a709ba98f462b00cd67a6d3aad90c
Dennis Costa <56238317+fcdennis@users.noreply.github.com>
2020-07-27 13:50:53 -0300

    ex095-096

--- mundo3/parte3/ex095.py
+++ mundo3/parte3/ex095.py
@@ -1,31 +1,44 @@
-jogador = dict()
-gol = list()
 time = list()
+jogador = dict()
+partidas = list()
 while True:
+    jogador.clear()
     jogador['nome'] = input('Nome do Jogador: ')
-    jogador['partidas'] = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
-    for c in range(0, jogador['partidas']):
-        gol.append(int(input(f'     Quantos gols na partida {c + 1}: ')))
-    jogador['gols'] = gol.copy()
-    jogador['total'] = sum(gol)
+    tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+    partidas.clear()
+    for c in range(0, tot):
+        partidas.append(int(input(f'    Quantos gols na partida {c+1}? ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
     time.append(jogador.copy())
-    gol.clear()
-    flag = ' '
     while True:
-        flag = input('Quer continuar? [S/N] ').upper().strip()[0]
-        if flag in 'SsNn':
+        resp = input('Quer continuar? [S/N] ').upper().strip()[0]
+        if resp in 'SN':
             break
-        else:
-            print('Por favor, digite "S" ou "N".')
-    if flag in 'Nn':
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
         break
-
-print('=' * 50)
-print('cod  Nome    Gols    total')
-for c, jogador in enumerate(time):
-    print(f'{c:>3} ', end=' ')
-    for v in jogador.values():
-        print(f'{str(v):<15}', end=' ')
+print('-=' * 30)
+print('cod ', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print('')
+print('-' * 40)
+for k, v in enumerate(time):
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
     print('')
-print('=' * 50)
-
+print('-' * 40)
+while True:
+    busca = int(input('Mostrar dados de qual jogador? (999 para parar) '))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print('ERRO! Não existe jogador com código {busca}!')
+    else:
+        print(f'    -- Levantamento do jogador {time[busca]["nome"]}:"')
+        for i, g in enumerate(time[busca]['gols']):
+            print(f'    No jogo {i+1} fez {g} gols.')
+    print('-' * 40)
+print('<<VOLTE SEMPRE>>')
