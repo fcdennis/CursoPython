20248faf9a842764dd57960a7d06dbacacdda3dd
Dennis Costa <56238317+fcdennis@users.noreply.github.com>
2020-07-28 12:35:27 -0300

    ex107

---
+++ mundo3/parte5/mod_ex107/moeda.py
@@ -0,0 +1,19 @@
+def metade(num):
+    return float(num / 2)
+
+
+def dobro(num):
+    return float(num * 2)
+
+
+def aumentar(num, ind):
+    return float(num + (num / 100 * ind))
+
+
+def diminuir(num, ind):
+    return float(num - (num / 100 * ind))
+
+
+def moeda(valor):
+    return f'R${valor}'
+
